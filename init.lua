0ddd913ea125da17b6e93a6e66324bd723f5cad9
Kj. <alexandrd542@gmail.com>
2025-05-27 13:07:41 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,60 +1,64 @@
--- Устанавливаем номер строк
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Устанавливаем 4 пробела вместо табуляции
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Включаем поддержку мыши
 vim.opt.mouse = "a"
 
--- Улучшаем поиск
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Включаем автозавершение команд
 vim.opt.wildmenu = true
 
--- Включаем подсветку синтаксиса
 vim.cmd('syntax on')
 vim.cmd('set termguicolors')
 
--- Подключаем packer
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim' -- Сам packer
-    use 'nvim-treesitter/nvim-treesitter' -- Улучшенная подсветка кода
-    use 'nvim-tree/nvim-web-devicons' -- Иконки
+    use 'wbthomason/packer.nvim'
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+    }
+    use 'nvim-tree/nvim-web-devicons'
     use 'nvim-lualine/lualine.nvim'
-    use 'lukas-reineke/indent-blankline.nvim' -- Отображение отступов
-    use 'nvim-lualine/lualine.nvim' -- Красивый статусбар
-    use 'nvim-telescope/telescope.nvim' -- Поиск файлов и строк
-    use 'neovim/nvim-lspconfig' -- Настройки LSP (поддержка языков)
-    -- use 'hrsh7th/nvim-cmp' -- Автодополнение
-    -- use 'hrsh7th/cmp-nvim-lsp' -- Источник LSP для автодополнения
-    use 'vim-scripts/fasm.vim' -- Поддержка синтаксиса FASM
-    use 'akinsho/bufferline.nvim' -- Вкладки сверху
-    use 'norcalli/nvim-colorizer.lua' -- Подсветка HEX цветов
-    use 'numToStr/Comment.nvim' -- Комментирование кода
-    use 'sainnhe/everforest'-- Тема оформления
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'vim-scripts/fasm.vim'
+    use 'akinsho/bufferline.nvim'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'numToStr/Comment.nvim'
+    use 'windwp/nvim-autopairs'
+    use 'sainnhe/everforest'
 end)
 
-
--- Тема
-
+vim.o.background = "dark"
 vim.cmd[[colorscheme everforest]]
-vim.o.background = "dark" -- темная тема
 
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "cpp", "python", "go", "rust", "bash" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
 
+require('Comment').setup()
+require('nvim-autopairs').setup{}
 
-
--- Настройка LSP
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup{} -- Поддержка Python
-lspconfig.gopls.setup{} -- Поддержка Go
-lspconfig.rust_analyzer.setup{} -- Поддержка Rust
-lspconfig.bashls.setup{} -- Поддержка Bash
-lspconfig.clangd.setup{} -- Поддержка C/C++
+
+lspconfig.pyright.setup{}
+lspconfig.gopls.setup{}
+lspconfig.rust_analyzer.setup{}
+lspconfig.bashls.setup{}
+lspconfig.clangd.setup{}
+
+require'colorizer'.setup()
+
+vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
